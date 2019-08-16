eb802a6ac7ad0efca8e4cfe1be6432c07c208bee
csnam_home <csnam515@gmail.com>
2019-08-17 03:43:08 +0900

    alert,comfirm prompt
    Signed-off-by: csnam_home <csnam515@gmail.com>

--- script_btn.js
+++ script_btn.js
@@ -1,4 +1,13 @@
-var hw = document.getElementById('hw');
-hw.addEventListener('click',function(){
-    alert('java script seperate method');
-})
+// var hw = document.getElementById('hw');
+// hw.addEventListener('click',function(){
+//     alert('java script seperate method');
+// })
+// header넣을 경우 
+window.onload =function(){
+    var hw = document.getElementById('hw');
+    hw.addEventListener('click',function(){
+        alert('header java script seperate method');
+    })
+}
+
+
